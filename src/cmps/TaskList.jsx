2cc3ae6bb1bfc8bb37e17c947b978e1bf008cafb
Keren Duchan <keren.duchan@gmail.com>
2023-12-12 09:31:15 +0200

    change bg color of task list being dragged over

--- src/cmps/TaskList.jsx
+++ src/cmps/TaskList.jsx
@@ -1,10 +1,11 @@
-import { forwardRef } from "react";
+import { forwardRef } from 'react'
 
 export const TaskList = forwardRef(function TaskList(props, ref) {
-  const { children } = props;
-  return (
-    <div ref={ref} className="tasks">
-      {children}
-    </div>
-  );
-});
+    const { className, children } = props
+    console.log('className', className)
+    return (
+        <div ref={ref} className={`task-list ${className ? className : ''}`}>
+            {children}
+        </div>
+    )
+})
